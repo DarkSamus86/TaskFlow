a9002acf5d1a49fbf7583966a325d8f8cb3f031c
Daniil Lobanov <daniillobanov1486@gmail.com>
2026-01-15 09:51:43 +0500

    Remove notification service (it because i will write it on Go).

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,12 @@
-import org.gradle.kotlin.dsl.internal.relocated.kotlin.metadata.internal.metadata.deserialization.VersionRequirementTable.Companion.create
-
 rootProject.name = "TaskFlow"
 
-include("AuthService")
-include("TaskService")
-include("NotificationService")
-include("Gateway")
+include("AuthService", "TaskService", "Gateway")
 
 dependencyResolutionManagement {
     versionCatalogs {
         create("springboot") {
-            library("bom", "org.springframework.boot:spring-boot-dependencies:4.0.1")
+            library("bom", "org.springframework.boot", "spring-boot-dependencies")
+                .version("4.0.1")
         }
     }
 }
